e8c7a983f8bea899f8827d466461aa5d31ea464b
Nurik <yanzip@yandex.ru>
2024-11-09 15:36:41 +0500

    Исп авто-я, сог-ных коммитов версион-я

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
-
-import style from './index.module.scss';
-import './index.scss';
-import './assets/images/dog.jpg';
-console.log(style); //использование объекта style важно для попадания стилей в css-бандл
+import style from './index.module.scss'
+import './index.scss'
+import './assets/images/dog.jpg'
+console.log(style) //использование объекта style важно для попадания стилей в css-бандл
